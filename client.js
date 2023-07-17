aa1785e1b228ad667e286048b796bd32f4f3114c
Gregory White <gregory.gm.white@gmail.com>
2023-07-16 17:04:53 -0700

    add basic styling from bootstrap

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 import { relayInit } from 'nostr-tools';
 
 
-const form = document.getElementById('connectForm');
+const form = document.getElementById('queryForm');
 const eventsDiv = document.getElementById('events');
 
 form.addEventListener('submit', function(e) {
@@ -18,10 +18,17 @@ form.addEventListener('submit', function(e) {
             eventsDiv.innerHTML = '';
 
             let events = await relay.list([{ kinds: [1] }]);
+
+            let countDiv = document.createElement('div');
+            countDiv.className = 'mb-2';
+            countDiv.textContent = `Matching events: ${events.length}`;
+            eventsDiv.appendChild(countDiv);
             for (let event of events) {
                 let eventDiv = document.createElement('div');
-                eventDiv.className = 'event';
-                eventDiv.textContent = JSON.stringify(event);
+                eventDiv.className = 'event card p-4 mb-4';
+                let eventCodeDiv = document.createElement('code');
+                eventCodeDiv.textContent = JSON.stringify(event, undefined, 2);
+                eventDiv.appendChild(eventCodeDiv);
                 eventsDiv.appendChild(eventDiv);
             }
         } catch (error) {
