d4c128708e52f9d22c9b56c91c998264269221ee
Gregory White <gregory.gm.white@gmail.com>
2023-07-16 21:47:45 -0700

    add support for querying specific kinds of events

--- client.js
+++ client.js
@@ -3,10 +3,13 @@ import { relayInit } from 'nostr-tools';
 
 const form = document.getElementById('queryForm');
 const eventsDiv = document.getElementById('events');
+const defaultLimit = 50;
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     const url = document.getElementById('relay').value;
+    const kindsSelectionRaw = getSelectValues(document.getElementById('kinds-selection'));
+    let kinds = parseKindsSelection(kindsSelectionRaw);
 
     // import and initialize the relay
     const relay = relayInit(url);
@@ -17,12 +20,35 @@ form.addEventListener('submit', function(e) {
             // Clear the contents of the events div
             eventsDiv.innerHTML = '';
 
-            let events = await relay.list([{ kinds: [1] }]);
+            // Add a loading message
+            let loadingEventsDiv = document.createElement('div');
+            loadingEventsDiv.className = 'mt-4';
+            loadingEventsDiv.textContent = 'Loading events...';
+            eventsDiv.appendChild(loadingEventsDiv);
 
+            let queries = [{ limit: defaultLimit }]
+
+            if (kinds.length !== 0) {
+                queries[0]["kinds"] = kinds;
+            }
+
+            // get events from the relay
+            let events = await relay.list(queries);
+
+            // clear the loading message
+            eventsDiv.innerHTML = '';
+
+            // Add a count of the events
             let countDiv = document.createElement('div');
             countDiv.className = 'mb-2';
-            countDiv.textContent = `Matching events: ${events.length}`;
+            if (events.length >= defaultLimit) {
+                countDiv.textContent = `Matching first ${events.length} events`;
+            } else {
+                countDiv.textContent = `Matching events: ${events.length}`;
+            }
             eventsDiv.appendChild(countDiv);
+
+            // Add the events as cards
             for (let event of events) {
                 let eventDiv = document.createElement('div');
                 eventDiv.className = 'event card p-4 mb-4';
@@ -35,6 +61,12 @@ form.addEventListener('submit', function(e) {
             }
         } catch (error) {
             console.error(`Error listing events: ${error.message}`);
+
+            // add error message
+            let errorDiv = document.createElement('div');
+            errorDiv.className = 'alert alert-danger';
+            errorDiv.textContent = `Error listing events: ${error.message}`;
+            eventsDiv.appendChild(errorDiv);
         }
     });
 
@@ -46,3 +78,33 @@ form.addEventListener('submit', function(e) {
     relay.connect();
 });
 
+// Return an array of the selected opion values
+function getSelectValues(select) {
+    var result = [];
+    var options = select && select.options;
+    var opt;
+
+    for (var i=0, iLen=options.length; i<iLen; i++) {
+        opt = options[i];
+
+        if (opt.selected) {
+        result.push(opt.value || opt.text);
+        }
+    }
+    return result;
+}
+
+
+function parseKindsSelection(selections) {
+    var result = [];
+    for (var i=0, iLen=selections.length; i<iLen; i++) {
+        let selection = selections[i];
+
+        if (selection === "All") {
+            return []
+        }
+
+        result.push(parseInt(selection));
+    }
+    return result;
+}
