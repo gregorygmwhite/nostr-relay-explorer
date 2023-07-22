3bbe925652313b0ff758542c4928c984a217bdd4
Gregory White <gregory.gm.white@gmail.com>
2023-07-21 18:45:31 -0700

    add some UX improvements to the events displayer

--- components/events.tsx
+++ components/events.tsx
@@ -8,17 +8,23 @@ const EventsDisplay = ({
     events: Event[];
   }): ReactElement => {
     return (
-        <div>
-            {events.map((event) => (
-              <div key={event.id} className="p-6 my-4 border border-gray-300 rounded shadow">
-                <pre className="overflow-auto break-all max-h-96">
-                  <code>
-                    {JSON.stringify(event, null, 2)}
-                  </code>
-                </pre>
-              </div>
-            ))}
-        </div>
+      <div>
+        {events.length > 0 && <div className="text-xl font-bold">First {events.length} Events</div>}
+        {events.length > 0 ? (
+          events.map((event) => (
+            <div key={event.id} className="p-6 my-4 border border-gray-300 rounded shadow">
+              <pre className="overflow-auto break-all max-h-96">
+                <code>
+                  {JSON.stringify(event, null, 2)}
+                </code>
+              </pre>
+            </div>
+          ))) : (
+            <div className="p-6 my-4 border border-gray-300 rounded shadow">
+              <p>No events found</p>
+            </div>
+        )}
+      </div>
     );
   };
 
