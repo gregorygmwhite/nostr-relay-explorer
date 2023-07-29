34c175a0ddf4c65209c4fcf6aafc6e63b2151888
Gregory White <gregory.gm.white@gmail.com>
2023-07-29 13:54:58 -0700

    add endpoint to retrieve relays individually

--- backend/explorer/urls.py
+++ backend/explorer/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('relays/', views.RelayListCreateView.as_view(), name='relay-list-create'),
+    path('relays/<uuid:id>/', views.RelayRetrieveView.as_view()),
 ]
--- backend/explorer/views/api/relays.py
+++ backend/explorer/views/api/relays.py
@@ -4,11 +4,16 @@ from explorer.serializers import RelaySerializer
 from explorer.tasks import update_metadata_for_relay
 
 
-
 class RelayListCreateView(generics.ListCreateAPIView):
-    queryset = Relay.objects.all()
+    queryset = Relay.objects.is_being_tracked()
     serializer_class = RelaySerializer
 
     def perform_create(self, serializer):
         instance = serializer.save()
         update_metadata_for_relay(instance.id)
+
+
+class RelayRetrieveView(generics.RetrieveAPIView):
+    lookup_field = 'id'
+    queryset = Relay.objects.is_being_tracked()
+    serializer_class = RelaySerializer
